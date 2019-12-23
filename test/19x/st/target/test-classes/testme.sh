b5cd46b4df52b1465081a79e78a80ba8d91b6784
Marc Parisi <phrocker@apache.org>
2019-12-23 13:20:43 -0500

    change dir for travis

--- test/19x/st/target/test-classes/testme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# mvn clean package && java -DinitScript="$PWD/src/main/resources/testme.sh"
-# -jar target/standalone-1.9.3-mac-shaded-0.0.1-SNAPSHOT.jar
-
-echo "Testing this mike"
-whoami
-ls -l /tmp
