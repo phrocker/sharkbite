e3c6a72a74816120b39809944ce5d5b97bf5e75b
Marc Parisi <phrocker@apache.org>
2020-01-08 07:16:57 -0500

    update py version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def operatingsystem():
 
 setup(
     name='sharkbite',
-    version='0.6.0.2',
+    version='0.6.0.3',
     author='Marc Parisi',
     author_email='phrocker@apache.org',
     url='https://www.github.com/phrocker/sharkbite',
