a68edc995b0362d0eb189f474b878218f724218d
Marc Parisi <phrocker@apache.org>
2020-05-15 21:01:58 -0400

    add requirements

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ html_theme_options = {
     'canonical_url': '',
     'analytics_id': '',  #  Provided by Google in your dashboard
     'logo_only': True,
-    'display_version': True,
+    'display_version': False,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
