2e8a479e3b1a090900cc4130c59368bc3ae012b3
Marc Parisi <phrocker@apache.org>
2020-06-22 17:05:24 -0400

    Fix setup

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def operatingsystem():
 
 setup(
     name='sharkbite',
-    version='0.6.3',
+    version='0.6.4',
     author='Marc Parisi',
     author_email='phrocker@apache.org',
     url='https://docs.sharkbite.io/',
