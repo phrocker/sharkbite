e6efe05010d2a8e3d370aadca116ea598abb371a
Marc Parisi <phrocker@apache.org>
2021-02-22 11:40:54 -0500

    Update python API

---
+++ examples/writerexample.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from ctypes import cdll
+from argparse import ArgumentParser
+from ctypes import cdll
+import ctypes
+import os
+import traceback
+import sys
+import time
+
+
+
+"""
+
+This is an Example of using the Python connectors. The example will accept user input
+create a table writing arbitrary information to it via the BatchWriter and scanner will put the written data      
+     
+             
+"""
+
+parser = ArgumentParser(description="This is an Apache Accummulo Python connector")
+
+parser.add_argument("-i", "--instance", dest="instance",
+                    help="Apache Accumulo Instance Name", required=True)
+parser.add_argument("-z", "--zookeepers", dest="zookeepers",
+                    help="Comma Separated Zookeeper List", required=True)
+parser.add_argument("-u", "--username", dest="username",
+                    help="User to access Apache Accumulo", required=True)
+parser.add_argument("-p", "--password", dest="password",
+                    help="Password to access Apache Accumulo. May also be supplied at the command line")
+parser.add_argument("-t", "--table", dest="table",
+                    help="Table to create/update")
+args = parser.parse_args()
+
+password = args.password
+table = args.table
+
+if not password:
+    print("Please enter your password")
+    password = input()
+    
+if not table:
+    table = "test"
+
+from sharkbite import *
+
+
+try:
+    writer = AccumuloWriter(args.instance,args.zookeepers,args.username,password,table,"")
+
+    writer.put("z",cf="cf",cq="cq")
+    writer.put("z",cf="cf2",cq="cq")
+
+    for keyvalue in writer.to_scanner().get("z"):
+        key = keyvalue.getKey()
+        value = keyvalue.getValue()
+        v = value.get()
+
+    
+except RuntimeError as e:
+     traceback.print_exc()
+     print("Oops, error caused: " + str(e))
--- sharkbite/__init__.py
+++ sharkbite/__init__.py
@@ -1,4 +1,144 @@
 import pkgutil
+import time
 __path__ = pkgutil.extend_path(__path__, __name__)
 
 from .pysharkbite import *
+
+class AccumuloBase:
+    _conf = None
+    _instance = ""
+    _zookeepers = ""
+    _password = ""
+    _username = ""
+    _zk = None
+    _user = None
+    _connector = None
+    _table = ""
+    _tableOperations = None
+    _auths = None
+
+    def to_scanner(self):
+        return AccumuloScanner(self._instance,self._zookeepers,self._username,self._password,self._table,self._auths)
+
+    def __init__(self):
+        pass
+
+class AccumuloScanner(AccumuloBase):
+    
+    def __init__(self, instance, zookepeers, username, password, table, auths):
+        pass
+
+class AccumuloWriter(AccumuloBase):
+    _row = ""
+    _mutation = None
+    _writer = None
+    
+    def __init__(self, instance, zookepeers, username, password, table, auths):
+        self._conf = Configuration()
+        self._instance = instance
+        self._zookeepers = zookepeers
+        self._username = username
+        self._password = password
+        self._zk =  ZookeeperInstance(self._instance, self._zookeepers, 1000, self._conf)
+        self._user = AuthInfo(self._username, self._password , self._zk.getInstanceId()) 
+        if isinstance(auths, Authorizations):
+            self._auths = auths
+        else:
+            if len(auths) > 0:
+                self._auths = Authorizations(auths)
+            else:
+                self._auths = Authorizations()
+        self._connector = AccumuloConnector(self._user, self._zk)
+        self._table = table
+        self._tableOperations = self._connector.tableOps(self._table)
+        self._writer = self._tableOperations.createWriter(self._auths,10)
+
+    def put(self,row, cf, cq, cv=None, timestamp=0, value=None):
+        if row != self._row:
+            self._row = row
+            if self._mutation is not None:
+                self._writer.addMutation(self._mutation)
+            self._mutation = Mutation(row)
+        if cf is None or cq is None:
+            raise ValueError("CF and CQ must be specified")
+        ts = timestamp
+        if ts == 0:
+            ts = milliseconds = int(round(time.time() * 1000))
+        if value is None:
+            if cv is None:
+                self._mutation.put(cf=cf,cq=cq,timestamp=ts) 
+            else:
+                self._mutation.put(cf=cf,cq=cq,cv=cv,timestamp=ts) 
+        else:
+            if cv is None:
+                self._mutation.put(cf=cf,cq=cq,cv="",timestamp=ts,value=value) 
+            else:
+                self._mutation.put(cf=cf,cq=cq,cv=cv,timestamp=ts,value=value) 
+    
+    def __del__(self):
+        if self._mutation is not None:
+            self._writer.addMutation(self._mutation)
+        self._writer.close()
+
+class AccumuloScanner(AccumuloBase):
+    _row = ""
+    _scanner = None
+    
+    def __init__(self, instance, zookepeers, username, password, table, auths):
+        self._conf = Configuration()
+        self._instance = instance
+        self._zookeepers = zookepeers
+        self._username = username
+        self._password = password
+        self._zk =  ZookeeperInstance(self._instance, self._zookeepers, 1000, self._conf)
+        self._user = AuthInfo(self._username, self._password , self._zk.getInstanceId()) 
+        if isinstance(auths, Authorizations):
+            self._auths = auths
+        else:
+            if len(auths) > 0:
+                self._auths = Authorizations(auths)
+            else:
+                self._auths = Authorizations()
+        self._connector = AccumuloConnector(self._user, self._zk)
+        self._table = table
+        self._tableOperations = self._connector.tableOps(self._table)
+        
+
+    def get(self,begin_row, end_row=None):
+        if end_row is None:
+            range = Range(begin_row)
+        else:
+            range = Range(begin_row,True,end_row,False)
+    
+
+        scanner = self._tableOperations.createScanner(self._auths,10)
+
+        scanner.addRange( range )
+    
+        return AccumuloIterator(scanner)
+
+    def __del__(self):
+        if self._scanner is not None:
+            self._scanner.close()
+
+class AccumuloIterator(AccumuloBase):
+
+    _scanner = None
+    _iter = None
+    _resultset = None
+
+    def __init__(self, scanner):
+        self._scanner = scanner
+        self._resultset = scanner.getResultSet()
+
+    def __iter__(self):
+        self._iter = self._resultset.__iter__()
+        return self
+
+    def __next__(self):
+        tk = self._iter.__next__()
+        if tk is None:
+            raise StopIteration
+        else:
+            return tk
+        
